5e33bfbda5f0aede3e791b40da560153a72604a3
Boris <bio-boris@github.com>
2023-05-23 14:24:06 -0500

    Add tests infra

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,4 @@ fi
 
     
 tests="test/narrative_k8_router/routes/*.py"
-PYTHONPATH=.:narrative_k8_router:test/narrative_k8_router pytest --cov=src --cov-report=xml $tests
+PYTHONPATH=.:narrative_k8_router:test/narrative_k8_router pytest --cov=narrative_k8_router --cov-report=xml $tests
