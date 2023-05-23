afa7b47e8145a8fbb35560a2f2b034c6d154851b
Boris <bio-boris@github.com>
2023-05-23 11:47:53 -0500

    Add tests infra

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+if [[ -n "$GITHUB_ACTIONS" ]]; then
+  pip install --upgrade pip
+fi
+
+    
 tests="test/narrative_k8_router/routes/*.py"
 PYTHONPATH=.:narrative_k8_router:test/narrative_k8_router pytest --cov=src --cov-report=xml $tests
